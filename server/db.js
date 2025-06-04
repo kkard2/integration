b09521f7d80df0fbfcdf3654450f419782143b9b
kkard2 <contact@kkard2.com>
2025-06-04 14:41:54 +0200

    use env vars for creds (it requires duplicate .env, maybe figure it out at some point

--- server/db.js
+++ server/db.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize"
+import dotenv from 'dotenv'
+dotenv.config()
 
-const sequelize = new Sequelize('integration', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+)
 
 export default sequelize
