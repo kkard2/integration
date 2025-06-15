a1a8a20380da65137c0534bfed10e2b3e3505899
jankess-v <jankestobog@gmail.com>
2025-06-15 20:36:48 +0200

    import/export json

--- client/src/components/ImportJson.jsx
+++ client/src/components/ImportJson.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import Navbar from "./Navbar.jsx";
+import '../App.css'
 
 export default function ImportJson() {
     const [fileData, setFileData] = useState(null);
@@ -44,6 +45,30 @@ export default function ImportJson() {
             <div className="container">
                 <h2>Wczytaj dane z pliku JSON</h2>
 
+                <div className="format-info">
+                    <p>
+                        Wymagany format pliku JSON:
+                        <br />
+                        &#123;
+                        &nbsp;
+                        <span className="key">"country"</span>: <span className="value">"POL"</span>, &nbsp;
+                        <span className="key">"data"</span>:
+                        <br />
+                        [&nbsp;&#123;
+                        <span className="key">"year"</span>: <span className="value">2001</span>,
+                        &nbsp;
+                        <span className="key">"value"</span>: <span className="value">8.05278</span>,
+                        &nbsp;
+                        <span className="key">"ratio"</span>: <span className="value">46.062</span>
+                        &#125;,
+                        ...
+                        ]
+                        &nbsp;
+                        &#125;
+                    </p>
+                </div>
+
+
                 <input type="file" accept=".json" onChange={handleFileUpload}/>
 
                 {error && <div style={{color: 'red'}}>{error}</div>}
@@ -56,34 +81,15 @@ export default function ImportJson() {
                                 width={700}
                                 height={350}
                                 data={chartData}
-                                margin={{
-                                    top: 5,
-                                    right: 20,
-                                    bottom: 25,
-                                    left: 25,
-                                }}
+                                margin={{top: 5, right: 20, bottom: 25, left: 25}}
                             >
                                 <CartesianGrid stroke="#ccc"/>
                                 <XAxis dataKey="year" label={{value: 'Rok', position: 'bottom', offset: 0}}/>
                                 <YAxis label={{value: '', angle: -90, dx: -20}}/>
                                 <Tooltip/>
                                 <Legend verticalAlign="top" height={36}/>
-                                <Line
-                                    type="monotone"
-                                    dataKey="value"
-                                    name="Spożyty alkohol (per capita) w L"
-                                    stroke="#2a6df4"
-                                    strokeWidth={2}
-                                    dot={{r: 3}}
-                                />
-                                <Line
-                                    type="monotone"
-                                    dataKey="ratio"
-                                    name="Procent zatrudnienia"
-                                    stroke="#f47c2a"
-                                    strokeWidth={2}
-                                    dot={{r: 3}}
-                                />
+                                <Line type="monotone" dataKey="value" name="Spożyty alkohol (per capita) w L" stroke="#2a6df4" strokeWidth={2} dot={{r: 3}}/>
+                                <Line type="monotone" dataKey="ratio" name="Procent zatrudnienia" stroke="#f47c2a" strokeWidth={2} dot={{r: 3}}/>
                             </LineChart>
                         </div>
                     </div>
